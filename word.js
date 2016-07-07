3c2faf19227f86975f88d9da519ac5561df65db1
Cecile Nguyen <cecile.ceng@gmail.com>
2016-07-07 18:04:05 -0500

    randomizing words

---
+++ word.js
@@ -0,0 +1,7 @@
+var wordList = require('./game.js'); //randomly pulled word from list
+var letterInput = [];
+
+word.toLowerCase().indexOf(letter.toLowerCase())
+
+var randoWord = (Math.floor(Math.random() * wordList.wordsToGuess.length);
+console.log(randoWord)

bbb83e804b47649676eba4e35aab67c998071c28
Cecile Nguyen <cecile.ceng@gmail.com>
2016-07-07 22:13:50 -0500

    working on switch states for guessing of letters

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
 var game = require("./game.js");
+var wordCons = require("./word.js");
+var letterCons = require("./letter.js");
 var inquirer = require("inquirer");
+var randoWord = game.randoWord;
+var bunchablanks = Array(randoWord.length+1).join('_ ');
+var letterGuessed;
 exports.letter;
 
-inquirer.prompt([{
-	name: 'letter',
-	type: 'text',
-	message: 'Enter a letter:',
-	validate: function(str){
-		var regEx = new RegExp("[a-zA-Z]")
-		return regEx.test(str)
-	}
-}]).then(function(letterInput){
-	var letter = letterInput.letter;
-}
+function guessLetters(guessesLeft) {
+	var guessesLeft = 10;
+	if (guessesLeft > 0) {
+		inquirer.prompt([{
+		type: 'input',
+		name: 'letter',
+		message: 'Enter a letter:'
+	}]).then(function(letterInput){
+		letterGuessed = letterInput.letter;
+		guessesLeft--;
+		switch(state) {
+			case "correctGuess":
+				if (index >= -1) && guessesLeft > 0 {
+					console.log("Nice job.")//or change the blanks to letters 
+				}
+				break;
+			case "incorrectGuess":
+				if (index = -1) && guessesLeft > 0 {
+					console.log(letterGuessed + " is not part of this word. Please try again.") 
+				}
+				break;
+			case "noGuessesLeft":
+				if (guessesLeft !<= 0) {
+					console.log("There are no more guesses. The end.")
+				}
+				break;
+			case "gotWordRight":
+				if (guessesLeft <= 0) {
+					console.log("That's right! Nice job!");
+				}
+				break;
+		}
+	console.log(letterGuessed);
+	console.log(randoWord + ', ' + letterGuessed + ': ' + findLetter(randoWord, letterGuessed));
+	console.log(bunchablanks);
+	})
+};
--- word.js
+++ word.js
@@ -1,50 +1,13 @@
 //Check the letters guessed versus the random word selected
-
-var gameRef = require('./game.js');
-var randoWord = gameRef.randoWord;
-
-function findLetter(word, letter, start)
-{
-  if(start==null) start=0;
-  var indexes = Array();
-  console.log(letter);
-  var index = word.toLowerCase().indexOf(letter.toLowerCase(),start);
-  if(index == -1)
-  {
-    console.log('This is the base case.' + letter + ' not found.');
-    return indexes;
-  }
-  else
-  {
-    console.log('Found ' + letter + ' at ' + index);
-    indexes.push(index);
-    indexes = indexes.concat(findLetter(word,letter,index+1));
-    console.log(indexes);
-    return indexes;
-  }
-}
-
-var bunchablanks = Array(randoWord.length+1).join('_ ');
-
-var inquirer = require("inquirer");
-//exports.letter;
-var letterGuessed;
-
-inquirer.prompt([{
-	type: 'input',
-	name: 'letter',
-	message: 'Enter a letter:'
-}]).then(function(letterInput){
-	letterGuessed = letterInput.letter;
-	console.log(letterGuessed);
-});
-
-console.log(randoWord + ', ' + letterGuessed + ': ' + findLetter(randoWord, letterGuessed));
-console.log(bunchablanks);
-//ban and return 2
-//banana 
-
-//var letterInput = [letter];
-
-//word.toLowerCase().indexOf(letter.toLowerCase())
-
+function findLetter(word, letter, start){
+	if(start==null) start=0;
+	var indexes = Array();
+	var index = word.toLowerCase().indexOf(letter.toLowerCase(),start);
+	if(index == -1) {
+		return indexes;
+	} else {
+	indexes.push(index);
+	indexes = indexes.concat(findLetter(word,letter,index+1));
+	return indexes;
+	}
+};
